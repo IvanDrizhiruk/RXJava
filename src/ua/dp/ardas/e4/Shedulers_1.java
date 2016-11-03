4cf8438090a85de9a264b2d1fd0222b5ba207856
Ivan Drizhiruk <ivan.drizhiruk@gmail.com>
2016-11-04 00:28:04 +0200

    Example of shedulers usage

--- src/ua/dp/ardas/e4/Shedulers_1.java
+++ src/ua/dp/ardas/e4/Shedulers_1.java
@@ -1,4 +1,41 @@
 package ua.dp.ardas.e4;
 
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Shedulers_1 {
+
+    public static void main(String[] args) {
+
+        List<String> datas = Arrays.asList("Java", "Javascript", "C#", "Go", "Scala");
+        Observable.from(datas)
+                .subscribeOn(Schedulers.io())
+                .doOnNext(data -> printThreadNameForSubscribe(data))
+                .observeOn(Schedulers.computation())
+                .subscribe(data -> printThreadNameForObserve(data));
+
+        sleep(7000);
+
+    }
+
+    private static void printThreadNameForSubscribe(String data) {
+        System.out.println("Subscribe " + data +" ==> " + Thread.currentThread().getName());
+        sleep(1000);
+    }
+
+    private static void printThreadNameForObserve(String data) {
+        System.out.println("Observe " + data +" ==> " + Thread.currentThread().getName());
+        sleep(500);
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/ua/dp/ardas/e4/Shedulers_2.java
+++ src/ua/dp/ardas/e4/Shedulers_2.java
@@ -1,4 +1,46 @@
 package ua.dp.ardas.e4;
 
+import rx.Observable;
+import rx.internal.schedulers.ExecutorScheduler;
+import rx.schedulers.Schedulers;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Shedulers_2 {
+
+    public static void main(String[] args) {
+
+        List<String> datas = Arrays.asList("Java", "Javascript", "C#", "Go", "Scala");
+
+        Observable.from(datas)
+                .flatMap(data -> Observable.just(data)
+                        .subscribeOn(Schedulers.io())
+                        .map(dataForPrint -> printThreadNameForSubscribe(dataForPrint))
+                )
+                .subscribe(dataForPrint -> printThreadNameForObserve(dataForPrint));
+
+        sleep(7000);
+    }
+
+    private static String printThreadNameForSubscribe(String data) {
+        System.out.println("Subscribe " + data +" ==> " + Thread.currentThread().getName());
+
+        sleep(500);
+
+        return data;
+    }
+
+    private static void printThreadNameForObserve(String data) {
+        System.out.println("Observe " + data +" ==> " + Thread.currentThread().getName());
+        sleep(500);
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
